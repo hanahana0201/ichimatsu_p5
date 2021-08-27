42d32ca55eab0b84911482094fc3779fc51a315d
Masatoshi Hanai <hanahana0201@gmail.com>
2021-08-27 11:36:06 +0900

    Add color variation.

--- sketch.js
+++ sketch.js
@@ -4,9 +4,17 @@ const d = w/s
 
 function setup() {
   createCanvas(w, w, SVG);
-  // O2 Light Gold
-  background(233, 203, 130);
-  // 01 Light Green
+
+  // 03 Light Red
+  background(15, 180, 225);
+
+  // // 03 Light Red
+  // background(245, 67, 65);
+
+  // // O2 Light Gold
+  // background(233, 203, 130);
+
+  // // 01 Light Green
   // background("#52b788");
 }
 
@@ -22,19 +30,24 @@ function draw() {
       }
     }
   }
-  save("ichimatsu_02.svg");
-  print("saved svg");
-  noLoop();
+  // save("ichimatsu_04.svg");
+  // print("saved svg");
+  // noLoop();
 }
 
 function drawRect(i,j){
   noStroke()
-  // 02 Dark Gold
 
+  // 04 Deep Blue
+  fill(7,160,220)
 
+  // // 03 Deep Red
+  // fill(200,18,17)
 
-  fill(240,213,136)
-  // 01 Dark Green
+  // // 02 Dark Gold
+  // fill(240,213,136)
+
+  // // 01 Dark Green
   // fill("#344e41")
   rect(i,j,d,d)
 }
